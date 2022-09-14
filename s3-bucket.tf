e23036b4fad5f6b6c186551b5626108845423f41
Liam Mehl <the.liam.mehl@gmail.com>
2022-09-14 05:13:47 +0000

    Fix bucket_prefix vs bucket conflict for s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/workshop-trial/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket = "gaurav-s3-bucket"
-  acl    = "private"
   bucket_prefix = "${var.prefix}"
-  versioning = {
-    enabled = true
-  }
 
 }
